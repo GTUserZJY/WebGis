bfb4f8c71792673e47d6b89e55d73e4f87fabfa8
GTUserZJY <3165926593@qq.com>
2025-04-16 16:24:08 +0800

    Initial commit: ArcGIS map app

---
+++ js/app.js
@@ -0,0 +1,89 @@
+require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/Legend",
+    "esri/widgets/ScaleBar",
+    "esri/widgets/Search",
+    "esri/widgets/LayerList",
+    "esri/layers/FeatureLayer",
+    "esri/Basemap",
+    "esri/widgets/Expand"
+], function(
+    Map, MapView, BasemapGallery, Legend, ScaleBar, 
+    Search, LayerList, FeatureLayer, Basemap, Expand
+) {
+    // 初始化地图
+    const map = new Map({
+        basemap: "streets-navigation-vector"
+    });
+
+    // 添加专题图层（示例使用ArcGIS Online上的公共图层）
+    const featureLayer = new FeatureLayer({
+        url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0",
+        title: "景观树木"
+    });
+    map.add(featureLayer);
+
+    // 创建地图视图
+    const view = new MapView({
+        container: "mapViewDiv",
+        map: map,
+        center: [-118.805, 34.027], // 初始中心点
+        zoom: 13
+    });
+
+    // 添加比例尺
+    const scaleBar = new ScaleBar({
+        view: view,
+        unit: "metric"
+    });
+    view.ui.add(scaleBar, {
+        position: "bottom-left"
+    });
+
+    // 添加搜索框
+    const searchWidget = new Search({
+        view: view
+    });
+    view.ui.add(searchWidget, {
+        position: "top-right",
+        index: 0
+    });
+
+    // 添加图例
+    const legend = new Legend({
+        view: view
+    });
+    view.ui.add(legend, "bottom-right");
+
+    // 添加图层列表
+    const layerList = new LayerList({
+        view: view
+    });
+    view.ui.add(layerList, "top-left");
+
+    // 添加底图库
+    const basemapGallery = new BasemapGallery({
+        view: view,
+        source: {
+            query: {
+                title: '"World Basemaps for Developers" AND owner:esri'
+            }
+        }
+    });
+    
+    // 将底图库放在底部区域
+    document.getElementById("basemapGalleryDiv").appendChild(basemapGallery.domNode);
+    
+    // 或者使用Expand小部件将底图库放在地图上
+    /*
+    const bgExpand = new Expand({
+        view: view,
+        content: basemapGallery,
+        expandIconClass: "esri-icon-basemap",
+        expandTooltip: "底图库"
+    });
+    view.ui.add(bgExpand, "top-right");
+    */
+});
